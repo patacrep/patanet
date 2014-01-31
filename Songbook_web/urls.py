b987ff2a9b00bff9a667d2d45e339282bbfa9460
Luthaf <luthaf@yahoo.fr>
2014-01-31 23:27:32 +0100

    Ajout du préfixe de langue dans les URLs

--- Songbook_web/urls.py
+++ Songbook_web/urls.py
@@ -3,7 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.conf.urls.i18n import i18n_patterns
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    )
+
+urlpatterns += i18n_patterns('',
     url(r'',include('generator.urls')),
-)
+    )
