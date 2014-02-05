0ddbb71db38178e032713eff4ef87d1e40467cd3
Luthaf <luthaf@yahoo.fr>
2014-02-05 22:16:09 +0100

    Removed debug settings from main settings file

--- Songbook_web/settings.py
+++ Songbook_web/settings.py
@@ -36,9 +36,6 @@ APPEND_SLASH = False
 ADMINS = (('Luthaf','luthaf@yahoo.fr'))
 
 DEFAULT_FROM_EMAIL = '' # webmaster@nom.du.site
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
-
 
 INSTALLED_APPS = (
     'django.contrib.admin',
