eeb9cb39d8e6c4f6465e1b3ccaa2af54a407a467
Oliverpool <oliverpool@hotmail.fr>
2014-02-13 19:12:40 +0100

    useless import

--- generator/models.py
+++ generator/models.py
@@ -9,7 +9,7 @@ from django.conf.global_settings import LANGUAGES
 from django.utils.translation import ugettext_lazy as _
 
 from jsonfield import JSONField
-from numpy import rank
+#from numpy import rank
 
 
 class Artist(models.Model):
