ee109d9258aa7d1314a1d302e8c45e231ca9b06d
Luthaf <luthaf@yahoo.fr>
2014-04-20 16:37:39 +0100

    Pylint

--- generator/management/commands/importsongs.py
+++ generator/management/commands/importsongs.py
@@ -12,7 +12,7 @@ from songbook_core.plastex import parsetex
 import pprint
 import git
 import os
-from generator import models
+from generator.models import Song, Artist
 from django.db import transaction
 from django.utils.text import slugify
 from django.utils.encoding import smart_text
@@ -41,20 +41,27 @@ class Command(BaseCommand):
             for filename in filenames:
                 if filename.lower().endswith(".sg"):
                     filepath = os.path.realpath(os.path.join(root, filename))
-                    filepath_rel = os.path.relpath(filepath, settings.SONGS_LIBRARY_DIR)
+                    filepath_rel = os.path.relpath(filepath,
+                                                   settings.SONGS_LIBRARY_DIR)
                     try:
                         data = parsetex(filepath)
-                        self.stdout.write("Processing " + pprint.pformat(data['titles'][0]))
+                        self.stdout.write("Processing " +
+                                          pprint.pformat(data['titles'][0]))
 
                         artist_name = smart_text(data['args']['by'], 'utf-8')
                         artist_slug = slugify(artist_name)
 
-                        artist_model, created = models.Artist.objects.get_or_create(slug=artist_slug, defaults={'name': artist_name})
+                        artist_model, created = Artist.objects.get_or_create(
+                                                slug=artist_slug,
+                                                defaults={'name': artist_name}
+                                                )
                         if not created:
                             if (artist_model.name != artist_name):
-                                self.stderr.write("*** Artist name differs though slugs are equal : "
-                                                  + artist_name + " / "
-                                                  + artist_model.name)
+                                self.stderr.write(
+                                    "*** Artist name differs though "
+                                    "slugs are equal : "
+                                    + artist_name + " / "
+                                    + artist_model.name)
 
                         if (len(data['languages']) > 1):
                             self.stderr.write("*** Multiple languages " +
@@ -63,36 +70,48 @@ class Command(BaseCommand):
                                               "Picking any.")
                         if (len(data['languages']) > 0):
                             language_name = data["languages"].pop()
-                            language_code = next((x for x in LANGUAGES if x[1].lower() == language_name.lower()), ('', ''))[0]
+                            language_code = next(
+                                        (x for x in LANGUAGES
+                                         if x[1].lower() == language_name.lower()
+                                         ),
+                                        ('', '')
+                                         )[0]
                             if language_code == '':
-                                self.stderr.write("*** No code found for language : '" + language_name + "'")
+                                self.stderr.write("*** No code found for "
+                                        "language : '" + language_name + "'")
 
                         song_title = smart_text(data['titles'][0], 'utf-8')
                         song_slug = slugify(song_title)
 
                         object_hash = gitcmd.hash_object(filepath)
 
-                        song_model, created = models.Song.objects.get_or_create(title=song_title,
-                                                                                artist=artist_model,
-                                                                                defaults={'title': song_title,
-                                                                                          'language': language_code,
-                                                                                          'object_hash': object_hash,
-                                                                                          'file_path': filepath_rel})
+                        song_model, created = Song.objects.get_or_create(
+                                                title=song_title,
+                                                artist=artist_model,
+                                                defaults={
+                                                'title': song_title,
+                                                'language': language_code,
+                                                'object_hash': object_hash,
+                                                'file_path': filepath_rel})
                         if created:
-                            if models.Song.objects.filter(slug=song_slug).exists():
+                            if Song.objects.filter(slug=song_slug).exists():
                                 song_slug += '-' + str(song_model.id)
                             song_model.slug = song_slug
 
                         else:
                             self.stdout.write("-> Already exists.")
                             if (song_model.title != song_title):
-                                self.stderr.write("*** Song names differs though slugs are equal : "
-                                                  + song_title + " / "
-                                                  + song_model.title)
+                                self.stderr.write(
+                                    "*** Song names differs though "
+                                    "slugs are equal : "
+                                    + song_title + " / "
+                                    + song_model.title)
 
                         artist_model.save()
                         song_model.save()
 
                     except IOError as e:
-                        self.stderr.write("*** Failed processing file : " + filepath)
-                        self.stderr.write("    I/O error({0}): {1}".format(e.errno, e.strerror))
+                        self.stderr.write("*** Failed processing file : "
+                                          + filepath)
+                        self.stderr.write("    I/O error({0}): {1}"
+                                          .format(e.errno, e.strerror))
