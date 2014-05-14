a5b37bc7495103a41e8dca4fa09435204381e0cc
Luthaf <luthaf@yahoo.fr>
2014-05-14 22:10:31 +0100

    Passage de l'integralite du code sous licence AGPL Fix #47

--- generator/urls.py
+++ generator/urls.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2014 The songbook Team
+#    Copyright (C) 2014 The Songbook Team
 #
-# This program is distributed under the terms of the GNU General
-# Public License as published by the Free Software Foundation;
-# either version 2 of the License, or any later version.
-# For more information, see the file LICENCE.txt
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.conf.urls import patterns, url
 from generator.views import SongList, SongView, SongListByArtist, ArtistList, \
