119723b2cafeb7323195db72784e353bf88a0641
Luthaf <luthaf@luthaf.fr>
2014-06-13 14:24:59 +0100

    Fin de l'organisation des carnets avec JS.

--- generator/static/js/sb_web.js
+++ generator/static/js/sb_web.js
@@ -1,13 +1,28 @@
 $(function() {
+  $(".no-js").hide();
+
   $( ".ordering" ).sortable({
       stop: function( event, ui ) {}
   });
 
-  //$(".ordering li input").css("display", "none");
+  $(".ordering li .item-rank").hide();
+  $('.remove').clone().css("display","inline").prependTo(".ordering li");
 
   $( ".ordering" ).on( "sortstop", function( event, ui ) {
-        $('.ordering').children('li').each(function(i) {
-            $(this).children('input').attr("value", i+1);
+        var i = 1;
+        $('.ordering').children('li').each(function() {
+            var input = $(this).children('.item-rank');
+            if (input.attr("value") != "X") {
+              input.attr("value", i);
+              i+=1;
+            }
         });
   });
+
+  $(".remove").click(function(e){
+      $(this).parent("li").children('input').attr("value", "X");
+      $(this).parent("li").hide();
+      $(".ordering").trigger("sortstop");
+  });
+
 });
