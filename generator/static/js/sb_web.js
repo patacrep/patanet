3df2d06c55d40db7196806b4b7551a4830a7e6ca
Luthaf <luthaf@luthaf.fr>
2014-06-13 15:34:35 +0100

    Séparation des fonctionalités dans des variables

--- generator/static/js/sb_web.js
+++ generator/static/js/sb_web.js
@@ -1,28 +1,32 @@
 $(function() {
   $(".no-js").hide();
 
-  $( ".ordering" ).sortable({
-      stop: function( event, ui ) {}
-  });
+  var ordering = function(){
+      // Make the ordering class sortable, with jquery-ui
+      $( ".ordering" ).sortable({
+          stop: function( event, ui ) {}
+      });
 
-  $(".ordering li .item-rank").hide();
-  $('.remove').clone().css("display","inline").prependTo(".ordering li");
+      $(".ordering li .item-rank").hide();
+      $('.remove').clone().css("display","inline").prependTo(".ordering li");
+      // Compute the rank for all the items
+      $( ".ordering" ).on( "sortstop", function( event, ui ) {
+            var i = 1;
+            $('.ordering').children('li').each(function() {
+                var input = $(this).children('.item-rank');
+                if (input.attr("value") != "X") {
+                  input.attr("value", i);
+                  i+=1;
+                }
+            });
+      });
+      // A click on remove add a 'X' in the input and hide it.
+      $(".remove").click(function(e){
+          $(this).parent("li").children('input').attr("value", "X");
+          $(this).parent("li").hide();
+          $(".ordering").trigger("sortstop");
+      });
+  }
 
-  $( ".ordering" ).on( "sortstop", function( event, ui ) {
-        var i = 1;
-        $('.ordering').children('li').each(function() {
-            var input = $(this).children('.item-rank');
-            if (input.attr("value") != "X") {
-              input.attr("value", i);
-              i+=1;
-            }
-        });
-  });
-
-  $(".remove").click(function(e){
-      $(this).parent("li").children('input').attr("value", "X");
-      $(this).parent("li").hide();
-      $(".ordering").trigger("sortstop");
-  });
-
+  ordering();
 });
