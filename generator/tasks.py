7d57a5ef379aad1c920126f9d43ebf55e1e4e4ae
Oliverpool <oliverpool@hotmail.fr>
2014-07-17 01:09:26 +0200

    Catch all songbook_generation errors

--- generator/tasks.py
+++ generator/tasks.py
@@ -33,7 +33,8 @@ def queue_render_task(task_id):
 
     try:
         filename = generate_songbook(task.songbook, task.layout)
-    except GeneratorError:
+    #except GeneratorError:
+    except:
         task.state = GeneratorTask.State.ERROR
         task.save()
         LOGGER.error("Failed task {0} (state : {1})"\
